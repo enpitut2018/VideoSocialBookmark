647ca784084bb62d873ff782d18e567046719b66
tomoyaf <gazimum@gmail.com>
2018-10-19 23:43:18 +0900

    Implement Ranking

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  def self.create_or_get(title, url)
+      Entry.exists?(url:  url) ? Entry.find_by(url: url) : Entry.create(title: title, url: url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-    resources :bookmarks
-    resources :entries
-    mount_devise_token_auth_for 'User', at: 'auth',
-      controllers: {
-      registrations: 'api/v1/auth/sign_up'
+      resources :bookmarks
+      resources :entries
+
+      get "/ranking/:page", to: "ranking#index"
+
+      mount_devise_token_auth_for 'User', at: 'auth',
+        controllers: {
+        registrations: 'api/v1/auth/sign_up'
       }
     end
   end
