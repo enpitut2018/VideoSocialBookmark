bdc600d0cc7f0a217534b6a5be5c6baf54c267f5
riku iwasaki <s1611426@u.tsukuba.ac.jp>
2018-10-19 16:06:28 +0900

    タブインデントの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-	namespace :api do
-		namespace :v1 do
-			resources :bookmarks
-		end
-	end
-	resources :bookmarks
-	root to:'bookmarks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :entries
-      mount_devise_token_auth_for 'User', at: 'auth',
-        controllers: {
-          registrations: 'api/v1/auth/sign_up'
-        }
+    resources :bookmarks
+    resources :entries
+    mount_devise_token_auth_for 'User', at: 'auth',
+      controllers: {
+      registrations: 'api/v1/auth/sign_up'
+      }
     end
   end
 end
