9685cba9b841356d077398e35c26658836f0cc01
oshibori <oshibori0121vox@gmail.com>
2018-10-24 14:50:15 +0900

    fixed Immutable Relation Error, and default routing '/trend'

--- app/controllers/api/v1/trend_controller.rb
+++ app/controllers/api/v1/trend_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::TrendController < ApplicationController
   before_action :set_trend, only: [:index]
 
-  # GET /ranking
+  # GET /trend
   def index
     render json: @trend
-    #p @ranking
+    #p @trend
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_trend
-      @trend= Entry.page(trend_params[:page])
-      #@ranking.each do |entry|
-      #  entry[:num_of_bookmarked] = entry.count_bookmarks
-      #end
-      @trend.reorder!(num_of_bookmarked: :desc)
+      
+      Entry.find_each do |entry|
+        entry[:num_of_bookmarked] = entry.count_bookmarks
+      end
+      @trend=Entry.all.reorder!(num_of_bookmarked: :desc)
     end
 
     # Only allow a trusted parameter "white list" through.
-    def trend_params
-      params.fetch(:entry, {}).permit(:page)
-    end
+    #def trend_params
+    #  params.fetch(:entry, {}).permit(:page)
+    #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :bookmarks
       resources :entries, :only => :show
-      get "/trend/:page", to: "trend#index"
+      get "/trend", to: "trend#index"
       post "/entries/:entry_id", to: "bookmarks#create_by_entry_id"
 
       mount_devise_token_auth_for 'User', at: 'auth',
