cceeaadd9d80ff93857e4075946a850a6cbb9419
tomoyaf <gazimum@gmail.com>
2018-10-28 02:49:23 +0900

    Update Star

--- app/controllers/api/v1/stars/entry_stars_controller.rb
+++ app/controllers/api/v1/stars/entry_stars_controller.rb
@@ -1,22 +1,22 @@
 class Api::V1::Stars::EntryStarsController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :set_entry_star, only: %i[destroy show]
+  before_action :set_entry_star, only: %i[show create destroy]
   before_action :authenticate_api_v1_user!, only: %i[create destroy show]
 
   # GET /stars/entries/:entry_id
   def show
-    if @entry_star.nil?
-      render json: { enabled: false }
-    else
-      render json: @entry_star
-    end
+    render json: { enabled: @entry_star.present? }
   end
 
   # POST /stars/entries/:entry_id
   def create
-    @entry_star = Entry.new(use_id: current_api_v1_user.id,
-                            entry_id: params[:entry_id])
+    if @entry_star.present?
+      redirect_to controller: 'api', action: 'routing_error'
+    end
+
+    @entry_star = EntryStar.new(user_id: current_api_v1_user.id,
+                                entry_id: params[:entry_id])
     if @entry_star.save
       render json: @entry_star
     else
