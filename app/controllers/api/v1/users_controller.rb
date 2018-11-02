f17969d76b0fc6da91e0ffe3dc8b659c9ddef1f5
ishowta <kinironote@gmail.com>
2018-11-02 22:33:43 +0900

    [refactoring] GET /current_user*
    `GET /current_user*`は現在のユーザーの情報を返すAPIなので認証は必要で、返すフィールドはuserのみでいい。

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,16 @@
 class Api::V1::UsersController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :authenticate_api_v1_user!, only: []
+  before_action :authenticate_api_v1_user!, only: %i[current_user_show current_user_icon]
 
   # GET /current_user
   def current_user_show
-    render json: current_api_v1_user
+    render json: current_api_v1_user, include: :user
   end
 
   # GET /current_user/icon
   def current_user_icon
-    render json: { "userIcon": { "url": current_api_v1_user.image } }
+    render json: { "userIcon": { "url": current_api_v1_user.image } } #  TODO : Implement user icon
   end
 
   # GET /users/:id
