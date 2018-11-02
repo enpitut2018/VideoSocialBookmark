d216d563d55306ea84f219769ad63c47b032e2a6
ishowta <kinironote@gmail.com>
2018-11-02 13:35:05 +0900

    テスト用のhelperを追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "TEST#{n}@example.com" }
+    sequence(:password) { |n| "PASSWORD#{n}" }
+    sequence(:password_confirmation) { |n| "PASSWORD#{n}" }
+  end
+end
---
+++ spec/support/request_helpers.rb
@@ -0,0 +1,28 @@
+module RequestSpecHelper
+  module JsonHelpers
+    def json
+      JSON.parse(response.body)
+    end
+  end
+
+  def login(user)
+    post "/api/v1/auth/sign_in", params: {format: "json", email: "TEST1@example.com", password: "PASSWORD1"}
+    @headers = response.headers
+    @headers = @headers.map { |k, v| [k, v] }.to_h
+    @headers.delete("Content-Type")
+  end
+
+  def snapshot(title, url, params = nil, black_list = [])
+    api_v1_url = "/api/v1" + url
+    if params
+      post api_v1_url, params: {format: "json", **params}, headers: @headers
+    else
+      get api_v1_url
+    end
+    data = JSON.parse(response.body)
+    black_list.each do |key|
+      data.delete(key.to_s)
+    end
+    expect(data.to_json()).to match_snapshot(api_v1_url + "---" + title)
+  end
+end
