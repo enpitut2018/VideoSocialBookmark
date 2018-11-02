c7cfb6c0190e779da26256b3f101a295b0ebe7f4
ishowta <kinironote@gmail.com>
2018-11-02 17:13:11 +0900

    外部キーにindexをつけた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_050251) do
+ActiveRecord::Schema.define(version: 2018_11_01_173723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_10_24_050251) do
     t.string "original_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_bookmarks_on_entry_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -30,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_050251) do
     t.integer "bookmark_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["bookmark_id"], name: "index_comments_on_bookmark_id"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -46,6 +49,8 @@ ActiveRecord::Schema.define(version: 2018_10_24_050251) do
     t.integer "entry_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_entry_stars_on_entry_id"
+    t.index ["user_id"], name: "index_entry_stars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
