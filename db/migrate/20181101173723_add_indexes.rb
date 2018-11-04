717f213320d109a811deb722f730ca818fb39542
tomoyaf <gazimum@gmail.com>
2018-11-04 22:48:22 +0000

    fix migration

--- db/migrate/20181101173723_add_indexes.rb
+++ db/migrate/20181101173723_add_indexes.rb
@@ -1,8 +1,8 @@
 class AddIndexes < ActiveRecord::Migration[5.2]
   def change
-    add_index :bookmarks, [:user_id, :entry_id]
-    add_index :comments, :bookmark_id,
+    add_index :bookmarks, %i[user_id entry_id]
+    add_index :comments, %i[user_id entry_id]
     add_index :entries, :num_of_bookmarked
-    add_index :entry_stars, [:user_id, :entry_id]
+    add_index :entry_stars, %i[user_id entry_id]
   end
 end
