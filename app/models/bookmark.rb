3cd47d06a0b01975aa1426807c66c34696c6b358
NotFounds <m134_warrock_rpg7@yahoo.com>
2018-11-14 14:36:59 +0900

    fix typo

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,7 +5,7 @@ class Bookmark < ApplicationRecord
   belongs_to :user
 
   after_save :update_entry_num_of_bookmarked
-  after_destory :update_entry_num_of_bookmarked
+  after_destroy :update_entry_num_of_bookmarked
 
   private
 
